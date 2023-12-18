550b542e5e013061d9f1c1c16e7ab29415d58343
Chris Bentley <chrisrbentley@pm.me>
2023-12-18 18:34:27 -0500

    Made header/nav responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,36 @@
 /* eslint-disable react/prop-types */
+import { useState } from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = ({ page }) => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
 	return (
 		<header className={styles.header}>
 			<h1>{page}</h1>
+			<nav>
+				<p
+					onClick={() => {
+						dropdownOpen ? setDropdownOpen(false) : setDropdownOpen(true);
+					}}
+				>
+					Menu
+				</p>
+				<ul
+					className={`${styles.dropdownMenu} ${
+						dropdownOpen ? styles.visible : ''
+					}`}
+				>
+					{page === 'Create Post' ||
+						(page === 'Posts' && (
+							<li>
+								<Link to={'/'}>Home</Link>
+							</li>
+						))}
+					<li>Log Out</li>
+				</ul>
+			</nav>
 		</header>
 	);
 };
